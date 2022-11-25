3500aade25d957bcce0105b9275a472008818ac2
Amedeo <amajer@student.hive.fi>
2022-11-25 17:42:22 +0200

    fdgfds

--- client/src/components/Stats.jsx
+++ client/src/components/Stats.jsx
@@ -2,6 +2,15 @@ import React, { useState, useEffect } from "react";
 import { getStats } from "../services/stats";
 import { getUserPhotos } from "../services/photos";
 
+const List = ({ list }) => {
+
+	return (
+		<div >
+			lol
+		</div>
+	);
+};
+
 const Stats = ({ target }) => {
 	const [views, setViews] = useState(0);
 	const [likes, setLikes] = useState(0);
@@ -35,39 +44,81 @@ const Stats = ({ target }) => {
 	if (done === true) {
 		return (
 			// css centre text and add padding to the top and bottom of the container //
-			<div className="container mt-3">
-				<h3 style={{ textAlign : 'center' }}>Your stats</h3>
-				<div className="row" style={{border : '2px solid black', textAlign : 'center'}}>
-					<div className="col-12" >
-						<p>{views} people viewed you:</p>
-						<div className="mb-3" style={{ maxHeight : '120px', overflowY : 'scroll'}}>
-							{viewHistory.map((user, index) => {
-								return (
-									<div key={index}>
-										<small>{user.name}</small><br></br>
+			<div>
+				<List list={viewHistory}/>
+				<div className="container mt-3">
+					<div className="row">
+						<div className="col-12">
+							<div className="card">
+								<div className="card-body">
+									<div className="row">
+										<div className="col-12">
+											<h3 className="text-center">*****</h3>
+										</div>
 									</div>
-								)
-							})}
-							</div>
-					</div>
-				</div>
-				<div className="row mt-3" style={{ textAlign : 'center', border : '2px solid black'}} >
-					<div className="col-12">
-						<p>{likes} people  liked you:</p>
-						<div className="mb-3" style={{ maxHeight : '120px', overflowY : 'scroll'}}>
-							{likeHistory.map((user, index) => {
-								return (
-									<div key={index}>
-										<small>{user.name}</small><br></br>
+									<div className="row">
+										<div className="col-12">
+											<div className="row">
+												<div className="col-12">
+													<img src={profilePic} alt="profile pic" className="img-fluid" />
+												</div>
+											</div>
+										</div>
 									</div>
-								)
-							})}
+									<div className="row">
+										<div className="col-12">
+											<div className="row">
+												<div className="col-12">
+													<h5 style={{ textDecoration : 'underline' }} className="text-center mt-3">{views} Views</h5>
+												</div>
+											</div>
+											<div className="row">
+												<div className="col-12">
+													<p className="text-center"></p>
+												</div>
+											</div>
+										</div>
+									</div>
+									<div className="row">
+										<div className="col-12">
+											<div className="row">
+												<div className="col-12">
+													<h5 style={{ textDecoration : 'underline' }} className="text-center">{likes} Likes</h5>
+												</div>
+											</div>
+											<div className="row">
+												<div className="col-12">
+													<p className="text-center"></p>
+												</div>
+											</div>
+										</div>
+									</div>
+									<div className="row">
+										<div className="col-12">
+											<div className="row">
+												<div className="col-12">
+													<h5 style={{ textDecoration : 'underline' }} className="text-center">{blocks} Blocks</h5>
+												</div>
+											</div>
+											<div className="row">
+												<div className="col-12">
+													<p className="text-center"></p>
+												</div>
+											</div>
+										</div>
+									</div>
+									<div className="row">
+										<div className="col-12">
+											<div className="row">
+												<div className="col-12">
+													<h5 style={{ textDecoration : 'underline' }} className="text-center">Matches</h5>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
 							</div>
-					</div>
-				</div>
-				<div className="row">
-					<div className="col-12 mt-2" style={{ textAlign : 'center', border : '2px solid black'}}>
-						<p>You have been blocked {blocks} times.</p>
+						</div>
 					</div>
 				</div>
 			</div>
