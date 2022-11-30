f986c5d56acfd041cbf19967a62c824f2b19562c
Kristian Ceder <krisuceder@hotmail.com>
2022-11-30 16:27:31 +0200

    Drop down menu onHover styling

--- client/src/components/settingsMenu.jsx
+++ client/src/components/settingsMenu.jsx
@@ -15,11 +15,11 @@ const Links = ({handleClick, setShowHide, showHide}) => {
 	return (
 		<div id="nav">
 			<Nav defaultActiveKey="/home" className="flex-column  align-items-center ">
-				<Nav.Link onMouseEnter={() => setHover1(true)} onMouseLeave={() => setHover1(false)} style={{backgroundColor : hover1 ? 'rgb(237, 237, 237)' : 'white', color: 'black'}} onClick={() => handleClick(1, setShowHide, showHide)}>Profile</Nav.Link>
-				<Nav.Link onMouseEnter={() => setHover2(true)} onMouseLeave={() => setHover2(false)} style={{backgroundColor : hover2 ? 'rgb(237, 237, 237)' : 'white', color: 'black'}} onClick={() => handleClick(4, setShowHide, showHide)}>Edit Profile</Nav.Link>
-				<Nav.Link onMouseEnter={() => setHover3(true)} onMouseLeave={() => setHover3(false)} style={{backgroundColor : hover3 ? 'rgb(237, 237, 237)' : 'white', color: 'black'}} onClick={() => handleClick(2, setShowHide, showHide)}>Password</Nav.Link>
-				<Nav.Link onMouseEnter={() => setHover4(true)} onMouseLeave={() => setHover4(false)} style={{backgroundColor : hover4 ? 'rgb(237, 237, 237)' : 'white', color: 'black'}} onClick={() => handleClick(3, setShowHide, showHide)}>Edit Photos</Nav.Link>
-				<Nav.Link onMouseEnter={() => setHover5(true)} onMouseLeave={() => setHover5(false)} style={{backgroundColor : hover5 ? 'rgb(237, 237, 237)' : 'white', color: 'black'}} onClick={() => handleClick(5, setShowHide, showHide)}>Scores</Nav.Link>
+				<Nav.Link onMouseEnter={() => setHover1(true)} onMouseLeave={() => setHover1(false)} style={{borderTop: hover1 ? '1px solid #e6e6e6' : 'none', borderBottom: hover1 ? '1px solid #e6e6e6' : 'none' ,backgroundColor : hover1 ? 'rgb(237, 237, 237)' : 'white', color: 'black'}} onClick={() => handleClick(1, setShowHide, showHide)}>Profile</Nav.Link>
+				<Nav.Link onMouseEnter={() => setHover2(true)} onMouseLeave={() => setHover2(false)} style={{borderTop: hover2 ? '1px solid #e6e6e6' : 'none', borderBottom: hover2 ? '1px solid #e6e6e6' : 'none' ,backgroundColor : hover2 ? 'rgb(237, 237, 237)' : 'white', color: 'black'}} onClick={() => handleClick(4, setShowHide, showHide)}>Edit Profile</Nav.Link>
+				<Nav.Link onMouseEnter={() => setHover3(true)} onMouseLeave={() => setHover3(false)} style={{borderTop: hover3 ? '1px solid #e6e6e6' : 'none', borderBottom: hover3 ? '1px solid #e6e6e6' : 'none' ,backgroundColor : hover3 ? 'rgb(237, 237, 237)' : 'white', color: 'black'}} onClick={() => handleClick(2, setShowHide, showHide)}>Password</Nav.Link>
+				<Nav.Link onMouseEnter={() => setHover4(true)} onMouseLeave={() => setHover4(false)} style={{borderTop: hover4 ? '1px solid #e6e6e6' : 'none', borderBottom: hover4 ? '1px solid #e6e6e6' : 'none' ,backgroundColor : hover4 ? 'rgb(237, 237, 237)' : 'white', color: 'black'}} onClick={() => handleClick(3, setShowHide, showHide)}>Edit Photos</Nav.Link>
+				<Nav.Link onMouseEnter={() => setHover5(true)} onMouseLeave={() => setHover5(false)} style={{borderTop: hover5 ? '1px solid #e6e6e6' : 'none', borderBottom: hover5 ? '1px solid #e6e6e6' : 'none' ,backgroundColor : hover5 ? 'rgb(237, 237, 237)' : 'white', color: 'black'}} onClick={() => handleClick(5, setShowHide, showHide)}>Scores</Nav.Link>
 			</Nav>
 		</div>
 )
